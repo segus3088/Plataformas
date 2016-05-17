4d7dbcd5653c075828f71b34a8d2e499c8ca1ba9
SEBASTIAN SALAZAR <segus3088@gmail.com>
2016-05-17 15:26:18 -0500

    algunos cambios

--- claseMayo17/haproxy.sh
+++ claseMayo17/haproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-Sudo DEBIAN_FRONTEND=noninteractive apt-get install -y haproxy
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y haproxy
 sudo sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/haproxy
 sudo mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bckp
 sudo cp -f /vagrant/haproxy.cfg /etc/haproxy/
--- claseMayo17/webserver.sh
+++ claseMayo17/webserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y apache2
-sudo sh -c "echo \<h1\>Hostname: $1\($2\)\<\/h1\> >> /var/www/html/index.html"
+sudo sh -c "echo \<h1\>Hostname: $1\($2\)\<\/h1\> >> /var/www/index.html"
