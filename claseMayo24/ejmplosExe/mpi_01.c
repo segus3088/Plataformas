0819cf95b606a27e24fa6f68849ac8d240064e23
Segus3088 <segus3088@gmail.com>
2016-06-07 00:25:57 -0500

    adicionando make para el mpi_01

--- claseMayo24/ejmplosExe/mpi_01.c
+++ claseMayo24/ejmplosExe/mpi_01.c
@@ -3,18 +3,19 @@
 #include <mpi.h>
 
 int main(int argc, char **argv) {
-	int my_rank, p, tag = 0;
+	int my_rank, p_size, tag = 0, dest=1, sourceM=0;
 	char msg[20];
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	MPI_Comm_size(MPI_COMM_WORLD, &p_size);
 	if (my_rank == 0) {
 		strcpy(msg, "Hello ");
-		MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
+		MPI_Send(msg, strlen(msg) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
 	} else if (my_rank == 1) {
-		MPI_Recv(msg, 20, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(msg, 20, MPI_CHAR, sourceM, tag, MPI_COMM_WORLD, &status);
+		printf("Mensaje recibido: %s\n", );
 	}
 	MPI_Finalize();
 }
